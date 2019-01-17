0473466e58d3efe4dbe47dca1ad4d4279645e803
Enki <870841070@qq.com>
2019-01-17 09:31:44 +0800

    基本界面

--- WPF界面Demo/MainWindow.xaml.cs
+++ WPF界面Demo/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Visifire.Charts;
 
 namespace WPF界面Demo
 {
@@ -22,6 +23,47 @@ namespace WPF界面Demo
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            topPanel.MouseDown += TopPanel_MouseDown;
+
+            LoadChart();
+        }
+
+        /// <summary>
+        /// 窗口拖动
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TopPanel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        void LoadChart()
+        {
+            chart.Titles.Clear();
+            chart.Series.Clear();
+            Visifire.Charts.Title title = new Visifire.Charts.Title();
+            title.Text = "释义常用分布图";
+            chart.Titles.Add(title);
+            DataSeries dataSeries = new DataSeries();
+            dataSeries.RenderAs = RenderAs.Column;
+            dataSeries.LabelEnabled = true;
+            dataSeries.LabelStyle = LabelStyles.OutSide;
+
+            DataPoint dataPoint;
+           
+            for (int i = 0; i < 10; i++)
+            {
+                dataPoint = new DataPoint();
+                dataPoint.AxisXLabel ="张三的分"+i;
+                dataPoint.YValue = i*2;
+                dataSeries.DataPoints.Add(dataPoint);
+            }
+            chart.Series.Add(dataSeries);
         }
     }
 }
